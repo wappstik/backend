1a09efe9e3b91e8fbdca59f99f372301526d9e68
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-07-07 12:26:34 +0700

    ref(auth): add 'guest' middleware to login and 'auth:sanctum' middleware to logout

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,12 @@ use App\Http\Controllers\auth\LogoutController;
 use App\Http\Controllers\auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', LoginController::class);
-Route::post('/logout', LogoutController::class);
+
+Route::middleware('guest')
+    ->post('/login', LoginController::class);
+    
 Route::post('/register', RegisterController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', LogoutController::class);
+});
