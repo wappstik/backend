27ac294f7dee0d5a331586b3b3338ea2326f0984
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-07-07 13:05:34 +0700

    feat(models): create models relationship on models class

--- app/Models/Pack.php
+++ app/Models/Pack.php
@@ -10,4 +10,19 @@ class Pack extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function stickers()
+    {
+        return $this->hasMany(Sticker::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Models/Sticker.php
+++ app/Models/Sticker.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Sticker extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function pack()
+    {
+        return $this->belongsTo(Pack::class);
+    }
 }
